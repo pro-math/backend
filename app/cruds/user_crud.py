e188c76d05498757a360fcc4f088a6555ae46d0f
ITSDENISSKA <fotiev.d1991@gmail.com>
2024-05-05 14:10:02 +0300

    do crud for user

---
+++ app/cruds/user_crud.py
@@ -0,0 +1,45 @@
+from sqlalchemy import select
+from sqlalchemy.engine import Result
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.models import User
+from app.schemas.user_schemas import UserCreate, UserUpdate, UserUpdatePartial
+
+
+async def get_users(session: AsyncSession) -> list[User]:
+    stmt = select(User).order_by(User.id)
+    result: Result = await session.execute(stmt)
+    users = result.scalars().all()
+    return list(users)
+
+
+async def get_user(session: AsyncSession, user_id: int) -> User | None:
+    return await session.get(User, user_id)
+
+
+async def create_user(session: AsyncSession, user_in: UserCreate) -> User:
+    user = User(**user_in.model_dump())
+    session.add(user)
+    await session.commit()
+    await session.refresh(user)
+    return user
+
+
+async def update_user(
+    session: AsyncSession,
+    user: User,
+    user_update: UserUpdate | UserUpdatePartial,
+    partial: bool = False,
+) -> User:
+    for name, value in user_update.model_dump(exclude_unset=partial).items():
+        setattr(user, name, value)
+    await session.commit()
+    return user
+
+
+async def delete_user(
+    session: AsyncSession,
+    user: User,
+) -> None:
+    await session.delete(user)
+    await session.commit()
