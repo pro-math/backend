bcb811cb218f2c7524e52e10a819c4fd1e510121
ITSDENISSKA <fotiev.d1991@gmail.com>
2024-05-05 15:39:05 +0300

    delete create tables on app start

--- app/main.py
+++ app/main.py
@@ -2,15 +2,12 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from app.models import db_helper, Base
 from app.views.game_sessions_views import game_sessions_router
 from app.views.users_views import users_router
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with db_helper.engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
     yield
 
 
