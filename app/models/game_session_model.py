3769ea3bc3ecacedf9815b69e21bada3dd62ca31
ITSDENISSKA <fotiev.d1991@gmail.com>
2024-05-05 16:10:00 +0300

    create relationships

--- app/models/game_session_model.py
+++ app/models/game_session_model.py
@@ -1,10 +1,17 @@
+from typing import TYPE_CHECKING
+
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.models import Base
 
+if TYPE_CHECKING:
+    from app.models import User
+
 
 class GameSession(Base):
+    __tablename__ = "game_sessions"
+
     game_mode: Mapped[str]
     examples_category: Mapped[int]
     total_count_examples: Mapped[int]
@@ -13,3 +20,4 @@ class GameSession(Base):
     user_id: Mapped[int] = mapped_column(
         ForeignKey("users.id"),
     )
+    user: Mapped["User"] = relationship(back_populates="game_sessions")
--- app/models/user_model.py
+++ app/models/user_model.py
@@ -1,7 +1,14 @@
-from sqlalchemy.orm import Mapped
+from typing import TYPE_CHECKING
+
+from sqlalchemy.orm import Mapped, relationship
 
 from app.models.base import Base
+
+if TYPE_CHECKING:
+    from app.models import GameSession
 
 
 class User(Base):
     username: Mapped[str]
+
+    game_sessions: Mapped[list["GameSession"]] = relationship(back_populates="user")
