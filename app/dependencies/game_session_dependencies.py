29f81f470491f9d467d89b947d1bde8c628b6050
ITSDENISSKA <fotiev.d1991@gmail.com>
2024-05-05 17:23:44 +0300

    create game session dependencies

---
+++ app/dependencies/game_session_dependencies.py
@@ -0,0 +1,25 @@
+from typing import Annotated
+
+from fastapi import Path, Depends, HTTPException, status
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.cruds import game_session_crud
+from app.models import db_helper
+from app.schemas import GameSession
+
+
+async def game_session_by_id(
+    game_session_id: Annotated[int, Path],
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+) -> GameSession:
+    game_session = await game_session_crud.get_game_session(
+        session=session,
+        game_session_id=game_session_id,
+    )
+    if game_session is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Game session {game_session_id} not found!",
+        )
+
+    return game_session
