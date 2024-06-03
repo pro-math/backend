0c8e38fce601ef1a0b248f1ba8a516abdc293d89
ITSDENISSKA <fotiev.d1991@gmail.com>
2024-06-03 12:04:05 +0300

    update game session

--- app/models/game_session_model.py
+++ app/models/game_session_model.py
@@ -1,6 +1,7 @@
+from datetime import datetime
 from typing import TYPE_CHECKING
 
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, DateTime, JSON, Integer, ARRAY, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.models import Base
@@ -12,12 +13,15 @@ if TYPE_CHECKING:
 class GameSession(Base):
     __tablename__ = "game_sessions"
 
-    game_mode: Mapped[str]
-    examples_category: Mapped[int]
-    total_count_examples: Mapped[int]
-    correct_count_examples: Mapped[int]
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
+    game_mode: Mapped[str] = mapped_column(String, nullable=False)
+    duration: Mapped[datetime] = mapped_column(DateTime, nullable=False)
+    math_operations: Mapped[list] = mapped_column(ARRAY(String), nullable=False)
+    examples_category: Mapped[int] = mapped_column(Integer, nullable=False)
+    examples: Mapped[dict] = mapped_column(JSON, nullable=False)
+    total_count: Mapped[int] = mapped_column(Integer, nullable=False)
+    correct_count: Mapped[int] = mapped_column(Integer, nullable=False)
+    created_at = mapped_column(DateTime, default=datetime.utcnow, nullable=False)
 
-    user_id: Mapped[int] = mapped_column(
-        ForeignKey("users.id"),
-    )
-    user: Mapped["User"] = relationship(back_populates="game_sessions")
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    user: Mapped["User"] = relationship("User", back_populates="game_sessions")
--- app/schemas/game_session_schemas.py
+++ app/schemas/game_session_schemas.py
@@ -1,31 +1,44 @@
-from pydantic import BaseModel, ConfigDict
+from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
 
 from app.schemas import GameMode
 
 
 class GameSessionBase(BaseModel):
     game_mode: GameMode
+    duration: datetime
+    math_operations: list[str]
     examples_category: int
+    examples: dict
     total_count: int
     correct_count: int
     user_id: int
 
 
-class GameSessionCreate(GameSessionBase): ...
+class GameSessionCreate(GameSessionBase):
+    pass
 
 
-class GameSessionUpdate(GameSessionCreate): ...
+class GameSessionUpdate(GameSessionBase):
+    pass
 
 
-class GameSessionUpdatePartial(GameSessionCreate):
-    game_mode: GameMode | None = None
-    examples_category: int | None = None
-    total_count: int | None = None
-    correct_count: int | None = None
-    user_id: int | None = None
+class GameSessionUpdatePartial(BaseModel):
+    game_mode: Optional[GameMode] = None
+    duration: Optional[datetime] = None
+    math_operations: Optional[list[str]] = None
+    examples_category: Optional[int] = None
+    examples: Optional[dict] = None
+    total_count: Optional[int] = None
+    correct_count: Optional[int] = None
+    user_id: Optional[int] = None
 
 
 class GameSession(GameSessionBase):
-    model_config = ConfigDict(from_attributes=True)
-
     id: int
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
