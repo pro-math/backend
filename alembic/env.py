29e5ce38d64ea86065cfd60ecc6d2ff5815f6bce
ITSDENISSKA <fotiev.d1991@gmail.com>
2024-06-05 16:43:33 +0300

    update database

--- alembic/env.py
+++ alembic/env.py
@@ -19,8 +19,8 @@ if config.config_file_name is not None:
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
-from app.models import Base
-from app.data import settings
+from src.models import Base
+from src.data import settings
 
 target_metadata = Base.metadata
 
