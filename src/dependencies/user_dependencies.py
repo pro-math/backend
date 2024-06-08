cb6af5d7ffdc65a53c66ebd00e6875e41a7f2c43
ITSDENISSKA <fotiev.d1991@gmail.com>
2024-06-08 11:25:22 +0300

    refactor dependencies

--- src/dependencies/user_dependencies.py
+++ src/dependencies/user_dependencies.py
@@ -5,10 +5,10 @@ from src.models.user_model import User
 from fastapi import Path, Depends, HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-
 from src.cruds import user_crud
 from src.models import db_helper
 from src.schemas import User
+from src.utils.auth import oauth2_scheme, verify_jwt_token
 
 
 async def user_by_id(
@@ -22,4 +22,17 @@ async def user_by_id(
             detail=f"User {user_id} not found!",
         )
 
+    return user
+
+
+async def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+) -> User | None:
+    decoded_data = verify_jwt_token(token)
+    if not decoded_data:
+        raise HTTPException(status_code=400, detail="Invalid token")
+    user = await user_crud.get_user(session=session, user_id=decoded_data["sub"])
+    if not user:
+        raise HTTPException(status_code=400, detail="User not found")
     return user
