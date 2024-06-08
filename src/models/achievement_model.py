931a393bb7e3b1b84749c115bc288b9e65880215
ITSDENISSKA <fotiev.d1991@gmail.com>
2024-06-08 11:23:07 +0300

    add achievement model

--- src/models/achievement_model.py
+++ src/models/achievement_model.py
@@ -1,14 +1,28 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy.orm import Mapped
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, relationship
 
 from src.models.base import Base
 
 if TYPE_CHECKING:
-    pass
+    from src.models import User
+
+user_achievements_table = Table(
+    "user_achievements",
+    Base.metadata,
+    Column("user_id", Integer, ForeignKey("users.id")),
+    Column("achievement_id", Integer, ForeignKey("achievements.id")),
+)
 
 
 class Achievement(Base):
     name: Mapped[str]
     description: Mapped[str]
     image: Mapped[str]
+
+    users: Mapped[list["User"]] = relationship(
+        secondary=user_achievements_table,
+        back_populates="achievements",
+        lazy="joined",
+    )
